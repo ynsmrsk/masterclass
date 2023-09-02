16258e334d90d408d82ce14e33f8172dd839f9ed
Yunus Emre Işık <ynsmrsk13@gmail.com>
2023-09-02 04:55:47 +0300

    fuck nextjs

--- components/generative-layout/index.jsx
+++ components/generative-layout/index.jsx
@@ -1,13 +1,16 @@
 import Desktop from './desktop'
 import Mobile from './mobile'
-import useIsTouchDevice from '@/hooks/use-is-touch-device'
 
 export default function GenerativeLayout({ data }) {
-	const isTouchDevice = useIsTouchDevice()
 
 	return (
-		isTouchDevice
-			? <Mobile data={data} />
-			: <Desktop data={data} />
+		<>
+			<div className='lg:hidden'>
+				<Mobile data={data} />
+			</div>
+			<div className='hidden lg:block'>
+				<Desktop data={data} />
+			</div>
+		</>
 	)
 }
