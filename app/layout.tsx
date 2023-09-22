441427bb87a421bac0614797ecedffc197eb3d9f
Yunus Emre Işık <ynsmrsk13@gmail.com>
2023-09-22 21:43:17 +0300

    up

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 					<meta property="og:title" content="Immersive Images" />
 					<meta property="og:description" content="İleri seviye 3dsmax & corona görselleştirme kursuna hoşgeldiniz!" />
 					<meta property="og:image" content="https://immersiveimages.co/og-image.jpg" />
-					<meta property="twitter:image" content="https://immersiveimages.co/og-image.jpg" />
 					<link rel="icon" href="../favicon.ico" />
 				</head>
 				<body className={`${fontBody.variable} ${fontDisplay.variable} font-body`}>
