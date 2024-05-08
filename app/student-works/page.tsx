1af821f5f3f8fe2e50eb134aa84f37d91f938420
Yunus Emre Işık <ynsmrsk13@gmail.com>
2024-05-08 20:58:44 +0300

    up

--- app/student-works/page.tsx
+++ app/student-works/page.tsx
@@ -1,7 +1,8 @@
 import GenerativeLayout from "./generative-layout"
+import { StudentWorks } from "@/sections"
 import { getStudentWorks } from "@/sanity/sanity-utils"
 
 export default async function Page() {
     const data = await getStudentWorks()
-    return <GenerativeLayout data={data} />
+    return <StudentWorks data={data} />
 }
