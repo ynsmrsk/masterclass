a508352b45ec524819e6422504f58c52b3e9f62d
Yunus Emre Işık <ynsmrsk13@gmail.com>
2024-12-03 04:15:17 +0300

    up

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
         <UsedPrograms />
         {/* <Numbers /> */}
         <Faq />
-        <CourseContent />
+        {/* <CourseContent /> */}
         <About />
         {/* <StackingText /> */}
         {/* <StackingTextClose /> */}
