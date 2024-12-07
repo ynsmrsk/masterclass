39aea19d6e00ae7ddbdfd9eac7821d7c75ff82a5
Yunus Emre Işık <ynsmrsk13@gmail.com>
2024-12-07 19:43:11 +0300

    update

--- sections/footer.jsx
+++ sections/footer.jsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 export default function Footer() {
   return (
-    <footer className="bg-black text-light font-display h-screen flex flex-col items-center justify-center overflow-hidden fixed bottom-0 w-full">
+    <footer className="fixed bottom-0 w-full h-screen bg-dark text-light font-display flex flex-col items-center justify-center contain-layout contain-paint">
       <Image src="/logo.avif" width={1920} height={1080} alt="Immersive Images logo" />
     </footer>
   )
