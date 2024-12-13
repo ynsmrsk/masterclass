69263fb6cdd8825fc464e834023152b73c115f37
oxomurat <oxomurat@gmail.com>
2024-12-13 20:16:01 +0300

    text changed

--- sections/about.tsx
+++ sections/about.tsx
@@ -83,22 +83,36 @@ export default function About() {
           height={1365}
         />
       </div>
-      <section id='content' className='container relative -mt-32 -mb-32 lg:translate-x-20 text-light min-h-screen max-w-[620px] text-balance flex flex-col'>
+      <section id='content' className='container relative -mt-32 -mb-32 lg:translate-x-20 text-light min-h-screen text-balance flex flex-col'>
         <h1 className='text-[66px] leading-none font-medium'>Selman Can</h1>
         <h2 className='text-2xl mb-6'>İç mimar & 3D görselleştirme uzmanı</h2>
-        <p className='text-2xl mb-4 text-primary-300'>3D görselleştirme alanında 7 yıldan fazla deneyime sahip ve aktif olarak mesleki alanın içerisindeyim.</p>
-        <p className='text-2xl text-primary-300'>Eğitimlerimde yazılımsal sırlarımı, fotogerçekçi sunum yöntemlerimi ve çalışmalarda kullandığım vazgeçilmez püf noktalarımı paylaşıyorum.</p>
+        <p className='text-2xl mb-4 text-primary-300'><b>Selman CAN</b>, 3D görselleştirme alanında 9 yıldan fazla deneyime sahip ve aktif
+          olarak mesleki alanın içindedir.</p>
+        <p className='text-2xl mb-4 text-primary-300'>
+          Kursta yazılımsal sırlarını, fotogerçekçi sunum yöntemlerini ve projelerinde kullandığı vazgeçilmez püf noktaları
+          paylaşıyor.
+        </p>
+        <p className='text-2xl mb-4 text-primary-300'>
+          <b>immersive.images<strong><sup><span style={{ fontFamily: 'system-ui' }} className='text-2xl'>&reg;</span></sup></strong></b>, Autodesk'in <b>Yetkili Eğitim Merkezi (<b>ATC<strong><sup><span style={{ fontFamily: 'system-ui' }} className='text-2xl'>&reg;</span></sup></strong></b>)</b>
+          ünvanını alarak 3ds Max eğitimlerini lisanslı bir şekilde vermeye başladı.
+          ABD'nin önde gelen yazılım şirketi Autodesk, <b>Selman CAN’ı</b> Öğrenme Partneri (Learning Partner) ailesine dahil etti.
+        </p>
+        <p className='text-2xl mb-4 text-primary-300'>
+          Autodesk’in yetkili eğitmeni <b>Selman CAN</b> ile <b>immersive.images<strong><sup><span style={{ fontFamily: 'system-ui' }} className='text-2xl'>&reg;</span></sup></strong></b>
+          tarafından düzenlenecek <p></p> 3ds Max eğitimleri, sektördeki profesyonellere
+          ve öğrencilere yönelik önemli bir gelişim fırsatı sunuyor.
+        </p>
         <div className='flex gap-8 md:gap-12 mt-10'>
           <Image
             src="/autodesk-authorized-training-center.avif"
             alt="Autodesk Authorized Training Center"
-            width={160}
+            width={180}
             height={41}
           />
           <Image
             src="/autodesk-learning-partner.avif"
             alt="Autodesk Learning Partner"
-            width={160}
+            width={180}
             height={41}
           />
         </div>
